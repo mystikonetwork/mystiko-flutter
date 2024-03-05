7a22a26fdca561a41f49e2591d026529bfe62258
moomarco <iamwumaixing@gmail.com>
2024-03-05 16:58:42 +0800

    update

--- scripts/build-other.sh
+++ scripts/build-other.sh
@@ -5,37 +5,23 @@ BUILD_DIR=platform-build
 mkdir $BUILD_DIR
 cd $BUILD_DIR || exit
 
-# Install build dependencies
-cargo install cargo-zigbuild
-cargo install cargo-xwin
-
-zig_build () {
-    local TARGET="$1"
-    local PLATFORM_NAME="$2"
-    local LIBNAME="$3"
-    rustup target add "$TARGET"
-    cargo zigbuild --target "$TARGET" -r
-    mkdir "$PLATFORM_NAME"
-    cp "../target/$TARGET/release/$LIBNAME" "$PLATFORM_NAME/"
+cross_build () {
+  local TARGET="$1"
+  local PLATFORM_NAME="$2"
+  local LIB_NAME="$3"
+  cross build --target "$TARGET" -r
+  mkdir "$PLATFORM_NAME"
+  cp "../target/$TARGET/release/$LIB_NAME" "$PLATFORM_NAME/"
 }
 
-win_build () {
-    local TARGET="$1"
-    local PLATFORM_NAME="$2"
-    local LIBNAME="$3"
-    rustup target add "$TARGET"
-    cargo xwin build --target "$TARGET" -r
-    mkdir "$PLATFORM_NAME"
-    cp "../target/$TARGET/release/$LIBNAME" "$PLATFORM_NAME/"
-}
+# Build for linux
+LINUX_LIB_NAME=libmystiko_lib_bridge.so
+cross_build x86_64-unknown-linux-gnu linux-x64 $LINUX_LIB_NAME
+cross_build aarch64-unknown-linux-gnu linux-arm64 $LINUX_LIB_NAME
 
-# Build all the dynamic libraries
-LINUX_LIBNAME=libmystiko_lib_bridge.so
-zig_build aarch64-unknown-linux-gnu linux-arm64 $LINUX_LIBNAME
-zig_build x86_64-unknown-linux-gnu linux-x64 $LINUX_LIBNAME
-WINDOWS_LIBNAME=mystiko_lib_bridge.dll
-win_build aarch64-pc-windows-msvc windows-arm64 $WINDOWS_LIBNAME
-win_build x86_64-pc-windows-msvc windows-x64 $WINDOWS_LIBNAME
+# Build for windows
+WINDOWS_LIB_NAME=mystiko_lib_bridge.dll
+cross_build x86_64-pc-windows-gnu windows-x64 $WINDOWS_LIB_NAME
 
 # Archive the dynamic libs
 tar -czvf mystiko_lib_bridge-other.tar.gz linux-* windows-*
