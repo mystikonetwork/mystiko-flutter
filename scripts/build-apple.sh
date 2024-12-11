34aef60c1a8aa015a260d68aecc15e51f46be069
X <96100679+moomarco@users.noreply.github.com>
2024-12-11 17:43:49 +0800

    update mystiko lib to latest (#25)
    * remove RUSTFLAGS when build release
    
    * update mystiko lib to latest
    
    * update

--- scripts/build-apple.sh
+++ scripts/build-apple.sh
@@ -12,7 +12,7 @@ for TARGET in \
 do
     rustup target add $TARGET
     # Apple's App Sandbox disallows SysV semaphores; use POSIX semaphores instead
-    RUSTFLAGS="-C link-args=-mios-version-min=10.0" cargo build -r --target=$TARGET
+    cargo build -r --target=$TARGET
 done
 
 # Create XCFramework zip
